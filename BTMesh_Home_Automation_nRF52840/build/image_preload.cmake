4e4e4b8a2ac5a935b68148c2831c4edcd13a42fb
saikalyan98 <52596098+saikalyan98@users.noreply.github.com>
2023-08-25 02:47:17 +0530

    BLE Mesh Home Automation
    1. Base code from nRF Connect SDK.
    2. Modified model handler.c by adding 4 more elements for holding switch status.

--- BTMesh_Home_Automation_nRF52840/build/image_preload.cmake
+++
@@ -1,7 +0,0 @@
-# Generated file that can be used to preload variant images
-set(BOARD "nrf52840dk_nrf52840" CACHE INTERNAL "NCS child image controlled")
-set(BOARD_ROOT "d:/workspace/nrfconnect/testing1/light_2_3/boards/arm/custom_home_automation;d:/workspace/nrfconnect/testing1/light_2_3;d:/workspace/nrfconnect/testing1/light_switch_2_3;d:/workspace/nrfconnect/testing1/light_2_3_test;d:/workspace/nrfconnect/testing1/btmesh_home_automation_nrf52840;C:/ncs/v2.3.0/nrf;C:/ncs/v2.3.0/zephyr" CACHE INTERNAL "NCS child image controlled")
-set(CACHED_CONF_FILE "d:/Workspace/nRFConnect/Testing1/BTMesh_Home_Automation_nRF52840/prj.conf" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "" CACHE INTERNAL "NCS child image controlled")
-set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
-set(WEST_PYTHON "C:\ncs\toolchains\v2.3.0\opt\bin\python.exe" CACHE INTERNAL "NCS child image controlled")
---
+++ BT_Mesh_Home_Automation/build/image_preload.cmake
@@ -0,0 +1,7 @@
+# Generated file that can be used to preload variant images
+set(BOARD "nrf52840dk_nrf52840" CACHE INTERNAL "NCS child image controlled")
+set(BOARD_ROOT "d:/workspace/nrfconnect/testing1/light_2_3/boards/arm/custom_home_automation;c:/users/hp/onedrive/desktop/freelancing/git_pushes/bluetooth_mesh/bt_mesh_home_automation;C:/ncs/v2.3.0/nrf;C:/ncs/v2.3.0/zephyr" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "c:/Users/Hp/OneDrive/Desktop/Freelancing/Git_Pushes/Bluetooth_Mesh/BT_Mesh_Home_Automation/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "C:\ncs\toolchains\v2.3.0\opt\bin\python.exe" CACHE INTERNAL "NCS child image controlled")
